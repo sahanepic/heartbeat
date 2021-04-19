ea7700ee0c70766668ce1dd75589cabe63598246
sahanbcs <sahanepic@gmail.com>
2021-04-19 21:43:50 +0530

    first commit

---
+++ app/src/main/java/com/example/heartbeat/HowToMeasureHeartRate.java
@@ -0,0 +1,14 @@
+package com.example.heartbeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToMeasureHeartRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_measure_heart_rate);
+    }
+}
